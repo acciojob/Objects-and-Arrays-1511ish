b705ce2caae75969be4ad670579c79ed3d301960
1511ish <117707870+1511ish@users.noreply.github.com>
2023-04-02 12:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+let team = players;
+let team1 = [];
+let i=0;
+players.foreach(fun);
+function fun(player) {
+	team1[i++] = player;
+}
 
+let cap1
 
 export {players,person,team,team1,cap1}
